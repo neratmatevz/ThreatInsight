2c26b4b565b4e8421739bf849c51ab9c9c47868c
aljazbracko <aljaz.bracko@student.um.si>
2024-05-13 23:42:30 +0200

    Stran za scan - začetni izgled

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Header from '../../components/Common/Header/Header';
-import Overview from './Introduction/Overview';
+import Overview from './Overview/Overview';
 import Features from './Features/Features';
 
 
--- frontend/src/Pages/ScansPage/ToolPicker/ToolPicker.tsx
+++ frontend/src/Pages/ScansPage/ToolPicker/ToolPicker.tsx
@@ -1,13 +1,32 @@
-// ToolPicker.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import './ToolPicker.css';
 
-const ToolPicker = () => {
+const ToolPicker: React.FC = () => {
+    const [visibleTool, setVisibleTool] = useState<string | null>(null);
+
+    const toggleTool = (toolName: string) => {
+        setVisibleTool(visibleTool === toolName ? null : toolName);
+    };
+
+    const tools = ["Nmap", "WhoIs", "HaveIBeenPwned", "IP Geolocation", "TLS/DNSSec Scan", "E-mail Verifier", "E-mail Permutator"];
+
     return (
         <div className="bg-light col-3 toolpicker-container">
-            {/* ToolPicker content */}
-            <h2 className="mb-2 fw-normal">ToolPicker</h2><hr></hr>
-            {/* Add your tools/options here */}
+            <h2 className="mb-2 fw-normal">ToolPicker</h2>
+            <hr></hr>
+            {tools.map((tool, index) => (
+                <div key={index} className="tool-entry">
+                    <div className="tool-item" onClick={() => toggleTool(tool)}>
+                        {tool}
+                        <span className={`arrow ${visibleTool === tool ? 'up' : 'down'}`}></span>
+                    </div>
+                    {visibleTool === tool && (
+                        <div className="tool-detail">
+                            <p>This is a placeholder for {tool} options and settings.</p>
+                        </div>
+                    )}
+                </div>
+            ))}
         </div>
     );
 }
