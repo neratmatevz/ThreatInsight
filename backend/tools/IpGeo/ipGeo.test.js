e14a245885a9352dff20cbffec763018525f7712
VerhovnikTeodor <74742395+VerhovnikTeodor@users.noreply.github.com>
2024-05-20 14:09:45 +0200

    test

--- backend/tools/IpGeo/ipGeo.test.js
+++ backend/tools/IpGeo/ipGeo.test.js
@@ -4,19 +4,18 @@ const path = require('path');
 
 
 describe('ipGeo function', () => {
-    const invalidIP = 'abc.def.ghi.jkl';
 
 
     test('throws an error if ipGeoData is not provided', async () => {
-        await expect(ipGeo(null, userUID, searchUID)).rejects.toThrow("IP for ipGeoData not provided!");
+        await expect(ipGeo(null)).rejects.toThrow("IP for ipGeoData not provided!");
     });
 
     test('throws an error if ipGeoData is not a string', async () => {
-        await expect(ipGeo(12345, userUID, searchUID)).rejects.toThrow("IP for ipGeoData must be a string!");
+        await expect(ipGeo(12345)).rejects.toThrow("IP for ipGeoData must be a string!");
     });
 
     test('throws an error if ipGeoData is not a valid IP address', async () => {
-        await expect(ipGeo(invalidIP, userUID, searchUID)).rejects.toThrow("Invalid IP address!");
+        await expect(ipGeo('abc.def.ghi.jkl')).rejects.toThrow("Invalid IP address!");
     });
 
   
