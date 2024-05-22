04c3f16c6fb3f346b4939d79b97ef3580fe14507
neratmatevz <matevz.nerat@gmail.com>
2024-05-22 21:37:07 +0200

    Popravljeni testi

--- backend/tools/DomainSearch/domainSearch.test.js
+++ backend/tools/DomainSearch/domainSearch.test.js
@@ -1,27 +1,19 @@
 const domainSearch = require('./domainSearch');
 
 describe('domainSearch parameter checking', () => {
-    test('throws an error if domainSearchJsonData is not provided', () => {
-        expect(() => {
-            domainSearch(null, 'userUID', 'searchUID');
-        }).toThrow('Parameters for domainSearch not provided!');
+    test('throws an error if domainSearchJsonData is not provided', async () => {
+        expect(domainSearch(null, 'userUID', 'searchUID')).rejects.toThrow('Parameters for domainSearch not provided!');
     });
 
-    test('throws an error if company is not provided', () => {
-        expect(() => {
-            domainSearch({ choosen: true }, 'userUID', 'searchUID');
-        }).toThrow('Domain cannot be empty or must be in format: domain.tld!');
+    test('throws an error if company is not provided', async () => {
+        expect(domainSearch({ choosen: true }, 'userUID', 'searchUID')).rejects.toThrow('Domain cannot be empty or must be in format: domain.tld!');
     });
 
-    test('throws an error if company is an empty string', () => {
-        expect(() => {
-            domainSearch({ choosen: true, company: '' }, 'userUID', 'searchUID');
-        }).toThrow('Domain cannot be empty or must be in format: domain.tld!');
+    test('throws an error if company is an empty string', async () => {
+        expect(domainSearch({ choosen: true, company: '' }, 'userUID', 'searchUID')).rejects.toThrow('Domain cannot be empty or must be in format: domain.tld!');
     });
 
-    test('throws an error if company is not in the correct format', () => {
-        expect(() => {
-            domainSearch({ choosen: true, company: 'invalid_domain' }, 'userUID', 'searchUID');
-        }).toThrow('Domain cannot be empty or must be in format: domain.tld!');
+    test('throws an error if company is not in the correct format', async () => {
+        expect(domainSearch({ choosen: true, company: 'invalid_domain' }, 'userUID', 'searchUID')).rejects.toThrow('Domain cannot be empty or must be in format: domain.tld!');
     });
 });
