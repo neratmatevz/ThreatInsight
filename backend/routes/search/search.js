14f0234f571de049186a2df52518a5ea8f791784
neratmatevz <matevz.nerat@gmail.com>
2024-05-27 17:59:29 +0200

    Pridobivanje končnega rezultata skena: endpoint, pridobivanje iz baze

--- backend/routes/search/search.js
+++ backend/routes/search/search.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { validateToken } = require('../../authentication/auth');
 const createSearch = require('../../search/createSearch/createSearch');
+const getSearch = require('../../search/getSearch/getSearch');
 
 let router = express.Router();
 
@@ -12,9 +13,32 @@ router.post('/', validateToken, async (req, res) => {
     } catch (error) {
         res.status(400).send(error.message);
     }
-    
+
     res.status(200).json(result);
 
+})
+
+router.get('/', validateToken, async (req, res) => {
+
+    try {
+        let userUID = req.body.userUID;
+        let searchUID = req.body.searchUID;
+
+        let scanData = await getSearch(userUID, searchUID);
+
+        res.status(200).json(scanData);
+
+    } catch (error) {
+
+        if (error.message === "Scan data for this search not found!") {
+
+            res.status(404).json({ message: error.message });
+
+        }
+
+        res.status(400).json({ message: "Bad request!" });
+
+    }
 })
 
 module.exports = router;
