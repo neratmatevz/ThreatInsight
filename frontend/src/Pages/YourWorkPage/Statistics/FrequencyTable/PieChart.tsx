e10bdc8c7ec14a5e8479ed217982d7ce532c0c6e
VerhovnikTeodor <74742395+VerhovnikTeodor@users.noreply.github.com>
2024-05-31 16:36:47 +0200

    logo, popravlen graf

--- frontend/src/Pages/YourWorkPage/Statistics/FrequencyTable/PieChart.tsx
+++ frontend/src/Pages/YourWorkPage/Statistics/FrequencyTable/PieChart.tsx
@@ -18,42 +18,53 @@ const BarChart: React.FC<BarChartProps> = ({ counts }) => {
     if (chartRef.current) {
       const ctx = chartRef.current.getContext("2d");
       if (ctx) {
+        const data = Object.keys(counts).map((key, index) => ({
+          label: key,
+          data: [counts[key]],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ][index % 6],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ][index % 6],
+          borderWidth: 1,
+        }));
+
         newChartInstance = new Chart(ctx, {
           type: 'bar',
           data: {
-            labels: Object.keys(counts),
-            datasets: [
-              {
-                label: '# of Counts',
-                data: Object.values(counts),
-                backgroundColor: [
-                  '#2BA8A4'
-                ],
-                borderColor: [
-                 '#2BA8A4'
-                ],
-                borderWidth: 1,
-              },
-            ],
+            labels: ['Counts'], // Single label for the dataset
+            datasets: data,
           },
           options: {
             plugins: {
               legend: {
-                display: false,
+                display: true,
                 position: 'top',
               },
             },
             scales: {
               x: {
                 beginAtZero: true,
+         
                 ticks: {
-                  color: '#e5e7eb', 
+                  color: '#e5e7eb',
                 },
               },
               y: {
                 beginAtZero: true,
                 ticks: {
-                  color: '#e5e7eb', 
+                  color: '#e5e7eb',
                 },
               },
             },
